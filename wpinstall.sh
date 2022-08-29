e4a65d69ad8704bd4f7d7f8dc1059c984d8cffef
waleedzafar68 <91959220+waleedzafar68@users.noreply.github.com>
2022-08-30 04:40:17 +0500

    A little test

--- wpinstall.sh
+++ wpinstall.sh
@@ -5,8 +5,6 @@ git clone https://github.com/waleedzafar68/vulnerablewp.git
 echo "Changing Directory"
 cd vulnerablewp
 echo "Running apt update"
-sudo apt update
-echo "Installing docker-compose"
-sudo apt install docker-compose -y
+sudo apt update && sudo apt install docker-compose -y
 echo "Running docker containers"
 docker-compose up -d
