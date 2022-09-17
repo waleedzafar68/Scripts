81e5ff4deceaec6ca913d31d6555b1069708de66
Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>
2022-09-17 06:13:27 +0500

    Adds Domain Services to a Windows Server
    Adds Domain Services to a Windows Server and Promotes the Server to Domain Controller
    
    Signed-off-by: Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>

---
+++ Add DS and Promote Server.ps1
@@ -0,0 +1,22 @@
+#Declare variables
+$DatabasePath = "c:\windows\NTDS"
+$DomainMode = "WinThreshold"
+$DomainName = "vtf.redteam.com"
+$DomaninNetBIOSName = "VTF"
+$ForestMode = "WinThreshold"
+$LogPath = "c:\windows\NTDS"
+$SysVolPath = "c:\windows\SYSVOL"
+$featureLogPath = "c:\featurelog.txt" 
+$Password = "dcpassword@VTF1"
+$SecureString = ConvertTo-SecureString $Password -AsPlainText -Force
+
+#Install AD DS, DNS and GPMC 
+start-job -Name addFeature -ScriptBlock { 
+Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools } 
+Wait-Job -Name addFeature 
+Get-WindowsFeature | Where installed >>$featureLogPath
+
+#Create New AD Forest
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $DatabasePath -DomainMode $DomainMode -DomainName $DomainName -SafeModeAdministratorPassword $SecureString -DomainNetbiosName $DomainNetBIOSName -ForestMode $ForestMode -InstallDns:$true -LogPath $LogPath -NoRebootOnCompletion:$false -SysvolPath $SysVolPath -Force:$true
