970e62ada707f44baf463488b8f59a4ba405b3e1
Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>
2023-11-14 21:46:20 +0500

    Update NewRelicChangeRecord.sh
    Signed-off-by: Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>

--- NewRelicChangeRecord.sh
+++ NewRelicChangeRecord.sh
@@ -1,1 +1,2 @@
-bash -c 'curl -X POST https://api.newrelic.com/graphql -H 'Content-type: application/json' -H 'API-Key: ${NEWRELIC_API_KEY}' -d '{ "query":  "{ requestContext { userId apiKey } }" }''
+echo "$NEWRELIC_API_KEY"
+curl -X POST https://api.newrelic.com/graphql -H 'Content-type: application/json' -H 'API-Key: ${NEWRELIC_API_KEY}' -d '{ "query":  "{ requestContext { userId apiKey } }" }'
