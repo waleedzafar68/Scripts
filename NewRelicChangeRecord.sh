4d9cf4999107439fa2997b5261153ca6450c391b
Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>
2023-11-15 18:15:26 +0500

    Update NewRelicChangeRecord.sh
    Signed-off-by: Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>

--- NewRelicChangeRecord.sh
+++ NewRelicChangeRecord.sh
@@ -2,4 +2,22 @@ echo "$NEWRELIC_API_KEY"
 curl -X POST https://api.newrelic.com/graphql \
      -H 'Content-Type: application/json' \
      -H "API-Key: ${NEWRELIC_API_KEY}" \
-     -d '{ "query": "mutation { changeTrackingCreateDeployment(deployment: { version: \"'"${RELEASE_TAG}"'\" user: \"'"${USER}"'\" timestamp: '"$(($(date +%s) * 1000))"' entityGuid: \"'"${NEWRELIC_ENTITY_GUID}"'\" description: \"'"${REPO}"'-'"${REPO_OWNER}"' commit '"${SHA}"' has been deployed successfully to Dev\" deploymentType: BASIC deepLink: \"'"${URL}"'\" commit: \"'"${SHA}"'\" changelog: \"'"${MESSAGE}"'\" }) { changelog commit deepLink deploymentId deploymentType description entityGuid timestamp user version }}" }'
+     -d @- << EOF
+{
+  "query": "mutation { 
+    changeTrackingCreateDeployment(deployment: { 
+      version: \"${RELEASE_TAG}\", 
+      user: \"${USER}\", 
+      timestamp: $(($(date +%s) * 1000)), 
+      entityGuid: \"${NEWRELIC_ENTITY_GUID}\", 
+      description: \"${REPO}-${REPO_OWNER} commit ${SHA} has been deployed successfully to Dev\", 
+      deploymentType: BASIC, 
+      deepLink: \"${URL}\", 
+      commit: \"${SHA}\", 
+      changelog: \"${MESSAGE}\" 
+    }) { 
+      changelog commit deepLink deploymentId deploymentType description entityGuid timestamp user version 
+    }
+  }"
+}
+EOF
