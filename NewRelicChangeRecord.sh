0b48b68d7074f443025abaed037f1e731c6c54e3
Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>
2023-11-15 16:57:05 +0500

    Update NewRelicChangeRecord.sh
    Signed-off-by: Waleed Zafar <91959220+waleedzafar68@users.noreply.github.com>

--- NewRelicChangeRecord.sh
+++ NewRelicChangeRecord.sh
@@ -1,2 +1,5 @@
 echo "$NEWRELIC_API_KEY"
-curl -X POST https://api.newrelic.com/graphql -H 'Content-type: application/json' -H "API-Key: ${NEWRELIC_API_KEY}" -d '{ "query":  "{ requestContext { userId apiKey } }" }'
+curl -X POST https://api.newrelic.com/graphql \
+     -H 'Content-Type: application/json' \
+     -H "API-Key: ${NEWRELIC_API_KEY}" \
+     -d "{ \"query\": \"mutation { changeTrackingCreateDeployment(deployment: { version: \\\"${RELEASE_TAG}\\\" user: \\\"${USER}\\\" timestamp: $(($(date +%s) * 1000)) entityGuid: \\\"${NEWRELIC_ENTITY_GUID}\\\" description: \\\"${REPO}-${REPO-OWNER} commit {SHA} has been deployeyed succesfuly to Dev\\\" deploymentType: BASIC deepLink: \\\"${URL}\\\" commit: \\\"${SHA}\\\" changelog: \\\"${MESSAGE}\\\" }) { changelog commit deepLink deploymentId deploymentType description entityGuid timestamp user version }}\" }" 
